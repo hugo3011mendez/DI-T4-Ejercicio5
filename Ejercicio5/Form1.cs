ad8b037e910c6e2af624d9d4e4881a580e492bb4
Hugo <hugo3011mendez@gmail.com>
2020-12-14 12:41:50 +0100

    Añadidos comentarios

--- Ejercicio5/Form1.cs
+++ Ejercicio5/Form1.cs
@@ -20,6 +20,8 @@ namespace Ejercicio5
             lblIndices.Text = "";
         }
 
+
+        // Función que contiene las acciones a realizar cuando se pulsa el botón añadir
         private void btnAnhadir_Click(object sender, EventArgs e)
         {
             if (txtAnhadir.Text == "")
@@ -32,33 +34,35 @@ namespace Ejercicio5
             }
         }
 
+
+        // Función que contiene las acciones a realizar cuando se pulsa el botón quitar
         private void btnQuitar_Click(object sender, EventArgs e)
         {
-            ListBox.SelectedObjectCollection seleccionados;
+            ListBox.SelectedObjectCollection seleccionados; // Creo una variable que recoge los items seleccionados de la listbox
 
-            if (listBox1.SelectedItems.Count == 0)
+            if (listBox1.SelectedItems.Count == 0) // Si la ListBox1 no tiene seleccionados...
             {
-                if (listBox2.SelectedItems.Count == 0)
+                if (listBox2.SelectedItems.Count == 0) // Si no hay seleccionados en ninguna ListBox...
                 {
-                    MessageBox.Show("No hay ningún elemento seleccionado!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("No hay ningún elemento seleccionado!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning); // Muestro mensaje de error
                 }
-                else
+                else // Si hay seleccionados en la ListBox2...
                 {
-                    seleccionados = listBox2.SelectedItems;
+                    seleccionados = listBox2.SelectedItems; // Establezco la variable seleccionados y la igualo a la colección de items seleccionados
 
-                    for (int i = 0; i < seleccionados.Count; i++)
+                    for (int i = 0; i < seleccionados.Count; i++) // Recorro los items seleccionados
                     {
-                        listBox2.Items.Remove(seleccionados[i]);
+                        listBox2.Items.Remove(seleccionados[i]); // Y los elimino de la ListBox correspondiente
                     }
                 }
             }
-            else
+            else // Si la ListBox1 tiene elementos seleccionados...
             {
-                seleccionados = listBox1.SelectedItems;
+                seleccionados = listBox1.SelectedItems; // Establezco la variable seleccionados y la igualo a la colección de items seleccionados 
 
-                for (int i = 0; i < seleccionados.Count; i++)
+                for (int i = 0; i < seleccionados.Count; i++) // Recorro los items seleccionados
                 {
-                    listBox1.Items.Remove(seleccionados[i]);
+                    listBox1.Items.Remove(seleccionados[i]); // Y los elimino de la ListBox correspondiente
                 }
             }
         }
